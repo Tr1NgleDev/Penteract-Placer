da21b14943dd99e888fdc48cce80481e83d5db7b
Mashpoe <mashpoesemail@gmail.com>
2026-03-13 18:07:51 -0600

    create world class that allocates a tesseractic array of chunks with a given edge length

---
+++ World.cpp
@@ -0,0 +1,50 @@
+#include "World.h"
+#include <glm/common.hpp>
+
+World::World(uint8_t edgeLength) :
+	edgeLength{ edgeLength },
+	chunks{ std::make_unique<Chunk[]>(edgeLength * edgeLength * edgeLength * edgeLength) }
+{
+	assert(edgeLength > 0);
+}
+
+size_t World::getEdgeLength() const
+{
+	return edgeLength;
+}
+
+Chunk* World::getChunks()
+{
+	return chunks.get();
+}
+
+const Chunk* World::getChunks() const
+{
+	return chunks.get();
+}
+
+Chunk* World::getChunk(glm::u8vec4 chunkPos)
+{
+	assert(glm::max(chunkPos, glm::u8vec4{ (uint8_t)(edgeLength - 1) }) == glm::u8vec4{ (uint8_t)(edgeLength - 1) });
+
+	size_t index =
+		(size_t)chunkPos.x + (edgeLength * edgeLength * edgeLength) +
+		(size_t)chunkPos.y + (edgeLength * edgeLength) +
+		(size_t)chunkPos.z + (edgeLength) +
+		(size_t)chunkPos.w;
+
+	return &chunks[index];
+}
+
+const Chunk* World::getChunk(glm::u8vec4 chunkPos) const
+{
+	assert(glm::max(chunkPos, glm::u8vec4{ (uint8_t)(edgeLength - 1) }) == glm::u8vec4{ (uint8_t)(edgeLength - 1) });
+
+	size_t index =
+		(size_t)chunkPos.x + (edgeLength * edgeLength * edgeLength) +
+		(size_t)chunkPos.y + (edgeLength * edgeLength) +
+		(size_t)chunkPos.z + (edgeLength)+
+		(size_t)chunkPos.w;
+
+	return &chunks[index];
+}
---
+++ World.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "Chunk.h"
+#include <memory>
+#include <glm/vec4.hpp>
+
+struct World
+{
+	World(uint8_t edgeLength);
+
+	size_t getEdgeLength() const;
+
+	// returns a pointer to the 4d array of chunks
+	Chunk* getChunks();
+	const Chunk* getChunks() const;
+
+	Chunk* getChunk(glm::u8vec4 chunkPos);
+	const Chunk* getChunk(glm::u8vec4 chunkPos) const;
+
+private:
+
+	uint8_t edgeLength;
+	std::unique_ptr<Chunk[]> chunks;
+};
